8039833b89d4d61f2730d89dce9e9f0a46aa2eb0
jpdf00 <j.pdf00@gmail.com>
2021-05-17 15:00:34 -0300

    Add button to restart game

--- src/scenes/gameOverScene.js
+++ src/scenes/gameOverScene.js
@@ -8,6 +8,7 @@ export default class GameOverScene extends Phaser.Scene {
 
   init(data) {
     this.score = data.score;
+    this.saved = false;
   }
 
   create (){
@@ -20,16 +21,52 @@ export default class GameOverScene extends Phaser.Scene {
     this.add.image(400, 300, 'frame');
 
     //Save Score
-    this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
-    this.centerButton(this.gameButton, 1);
+    this.saveButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.saveButton, 1);
 
-    this.gameText = this.add.text(0, 0, "Save", { fontSize: '32px', fill: '#fff' });
-    this.centerButtonText(this.gameText, this.gameButton);
+    this.saveText = this.add.text(0, 0, "Save Score", { fontSize: '24px', fill: '#fff' });
+    this.centerButtonText(this.saveText, this.saveButton);
 
-    this.gameButton.on('pointerdown', pointer => {
-      saveScore(input.value, this.score).then(resp => {
-        console.log(resp.result)
-      });
+    this.saveButton.on('pointerdown', pointer => {
+      if (!this.saved) {
+        saveScore(input.value, this.score).then(resp => {
+          this.updateText = this.add.text(400, 100, `${resp.result}`, { fontSize: '32px', fill: '#fff' });
+          this.updateText.setOrigin(0.5, 0.5);
+          this.saved = true;
+        });
+      } else {
+          this.updateText.destroy();
+          this.updateText = this.add.text(400, 100, `Score Alredy Saved`, { fontSize: '32px', fill: '#fff' });
+          this.updateText.setOrigin(0.5, 0.5);
+        }
+      input.value = '';
+      div.removeAttribute('Style')
+    });
+
+    //Replay Score
+    this.replayButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.replayButton, 0);
+
+    this.replayText = this.add.text(0, 0, "Replay", { fontSize: '32px', fill: '#fff' });
+    this.centerButtonText(this.replayText, this.replayButton);
+
+    this.replayButton.on('pointerdown', pointer => {
+      input.value = '';
+      div.removeAttribute('Style')
+      this.scene.start('Game');
+    });
+
+    //Leaderboard Score
+    this.leaderboardButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.leaderboardButton, -1);
+
+    this.leaderboardText = this.add.text(0, 0, "Leaderboard", { fontSize: '24px', fill: '#fff' });
+    this.centerButtonText(this.leaderboardText, this.leaderboardButton);
+
+    this.leaderboardButton.on('pointerdown', pointer => {
+      input.value = '';
+      div.removeAttribute('Style')
+      this.scene.start('Game');
     });
 
     this.input.on('pointerover', function (event, gameObjects) {
